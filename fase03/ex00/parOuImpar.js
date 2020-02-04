7b2a86a52a480b2480354f609c7270a5644d71a3
samanthasantos <59872468+samanthasantos@users.noreply.github.com>
2020-02-04 10:25:37 -0300

    ex2

--- fase03/ex00/parOuImpar.js
+++ fase03/ex00/parOuImpar.js
@@ -1,9 +1,10 @@
-function parOuImpar(numero){
-    var num = numero
-    var resto = numero %2
-    if(resto == 0) {
-        return ("par");
+function pares(num1, num2){
+    var numero = (num1, num2)
+     var resto = numero %2
+    if (resto == 0) {
+        return ("Os números são pares");
     }else {
-        return("impar")
+        return("Os números não são pares")
     }
+    
 }
