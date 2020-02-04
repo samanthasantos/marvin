599e24af5be0f2a67dc7d481e38bfc08473976ae
samanthasantos <59872468+samanthasantos@users.noreply.github.com>
2020-02-04 11:10:45 -0300

    ex2..

--- fase03/ex02/pares.js
+++ fase03/ex02/pares.js
@@ -1,7 +1,7 @@
-function pares(numeros){
-    var num = numeros
-     var resto = num %2
-    if (resto = 0) {
+function pares(num1, num2){
+    var numero = (num1, num2)
+     var resto = numero %2
+    if (resto == 0) {
         return ("Os números são pares");
     }else {
         return("Os números não são pares")
