8ddcaa7a55a80eeaa757c68fc894f17854732fb2
samanthasantos <59872468+samanthasantos@users.noreply.github.com>
2020-02-04 11:28:52 -0300

    ftxfas

--- fase03/ex02/pares.js
+++ fase03/ex02/pares.js
@@ -1,10 +1,8 @@
 function pares(num1, num2){
-    var numero = (num1, num2)
-     var resto = numero %2
-    if (resto == 0) {
-        return ("Os números são pares");
-    }else {
-        return("Os números não são pares")
+   
+    if (num1 % 2 === 0 && num2 % 2 === 0){
+        return ("Os números são pares")
+    } else{
+        return ("Os números não são pares")
     }
-    
 }
