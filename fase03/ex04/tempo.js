fa6a8d7a9dc5000cdd4a91eafaab7656cd926b9a
samanthasantos <59872468+samanthasantos@users.noreply.github.com>
2020-02-04 09:55:55 -0300

    ex4

---
+++ fase03/ex04/tempo.js
@@ -0,0 +1,21 @@
+function tempo(data1, data2, unidade){
+    var dataIni = new Date (data1);
+    var dataFim = new Date (data2);
+    switch(unidade){
+        case "dia":
+            var periodo = Math.abs(dataIni.getTime() -
+            dataFim.getTime())/100/60/60/24;
+            return Math.round(periodo);
+            break;
+            case "mês":
+                var periodo = Math.abs(dataIni.getTime() -
+                dataFim.getTime())/1000/60/60/24/30;
+                return Math.round(periodo);
+                default:
+                var periodo = Math.abs(dataIni.getTime() -
+                dataFim.getTime())/1000/60/60/24/30/12;
+                return Math.round(periodo);
+                break;
+    }
+}
+
