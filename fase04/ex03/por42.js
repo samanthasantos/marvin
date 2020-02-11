4d8d275f7bc47c7d67451393b9a69f93f15d2868
samanthasantos <59872468+samanthasantos@users.noreply.github.com>
2020-02-11 10:44:30 -0300

    ex3

--- fase04/ex03/por42.js
+++ fase04/ex03/por42.js
@@ -1,22 +1,12 @@
 function por42(num1, num2){
-    var mult = 42
-    var cont = 2
-    var flag = 0
     var result = null
-    while (mult < num2){
-        if(mult > num1){
-            if (flag == 1){
-                result = mult
-            } else {
-                flag = 1
-            }
+    while ( num1 <= num2){
+        if ( num1 % 42 === 0){
+            return Math.max(num1+42)
         }
-        mult = 42 * cont
-        cont++
-    }
-    if (!result){
-        console.log("Não encontrado");
-        return false
-    }
-    return result;
-}
+        num1++
+    } if (!result)
+    console.log("NÃ£o encontrado")
+    return false
+} 
+
