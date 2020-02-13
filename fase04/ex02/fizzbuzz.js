851c2ea2558d616a30e4b6efd82791cab188a218
samanthasantos <59872468+samanthasantos@users.noreply.github.com>
2020-02-13 09:16:25 -0300

    e2

--- fase04/ex02/fizzbuzz.js
+++ fase04/ex02/fizzbuzz.js
@@ -3,18 +3,20 @@ function fizzbuzz(num1, num2){
     while (num1 <= num2){
         
         if (num1 % 3 === 0 && num1 % 5 === 0){
-            console.log(num1 + " fizzbuzz");
+            console.log(num1 + " FizzBuzz");
             
         }
         else if (num1 % 3 === 0){
-            console.log(num1 + "fizz");
+            console.log(num1 + "Fizz");
             
         }
         else if (num1 % 5 === 0){
-            console.log(num1 + "buzz");
-            num1++
-        } else
-        console.log(num1)
+            console.log(num1 + "Buzz");
+        } else{
+             console.log(num1)
+        
+            
+        }
         num1++
     }
 }
