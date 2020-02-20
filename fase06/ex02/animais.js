de0508655a327e662fd8e8f958bda449c629a49e
samanthasantos <59872468+samanthasantos@users.noreply.github.com>
2020-02-20 11:11:53 -0300

    animais

---
+++ fase06/ex02/animais.js
@@ -0,0 +1,15 @@
+function animais(array){
+    var obj = array;
+    var novalidade = 5;
+    var i = 0;
+    
+    while (i< array.length) {
+        var posicao = obj [i];
+        delete podicao.altura
+        if (posicao.idade !== null) {
+            posicao.idade = novaIdade}
+            novaIdade++;
+    }
+    return array
+}
+
